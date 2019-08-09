914ad592f13cb31908f752e11bd662dfca3988d7
maddiestrange <maddie.strange@gmail.com>
2019-08-09 12:45:07 -0500

    added some testing for App rendering and an arbirary function, MVP complete?

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const help = require('./App');
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = rtl.render(
+    <App />
+    );
+  })
 });
+
+
+describe('sum function', () => {
+  it('sums two integers', () => {
+    const expected = 3;
+    const actual = help.sum(1, 2);
+    expect(actual).toBe(expected);
+  });
+}); 
--- client/src/index.js
+++ client/src/index.js
@@ -1,50 +1,7 @@
 import React from 'react';
-import FormikLoginForm from './Form';
-import './App.css';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import FoodCards from './components/FoodCards';
+import './index.css';
+import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    food: [{
-        name: 'hello'
-    }
-    ]
-    };
-  }
-
-  componentDidMount() {
-    this.callAPI();
-    console.log('food:', this.state)
-  }
-
-  callAPI = () => {
-    axios
-    .get(`http://localhost:5000/api/restricted/data`)
-      .then(data => {
-        this.setState({
-            food: data.data
-            })
-            console.log(this.state)
-        })
-        .catch(err => {
-            console.log(err);
-        });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <FormikLoginForm />
-        <FoodCards food={this.state.food}/>
-      </div>
-    );
-  }
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
